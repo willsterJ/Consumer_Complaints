448c8bb627c66adcecd5e42b1239df5718f82860
willsterJ <wj419@nyu.edu>
2020-07-21 17:46:22 -0400

    added more test

--- test_runs.sh
+++ test_runs.sh
@@ -6,3 +6,6 @@ python3 -m src.consumer_complaints --input ./insight_testsuite/tests/test_2/inpu
 echo -e '\n'
 sleep 1
 python3 -m src.consumer_complaints --input ./insight_testsuite/tests/test_3/input/missingheadings.csv --output ./insight_testsuite/tests/test_3/output/report.csv
+echo -e '\n'
+sleep 1
+python3 -m src.consumer_complaints --input ./insight_testsuite/tests/test_4/input/badcells.csv --output ./insight_testsuite/tests/test_4/output/report.csv
